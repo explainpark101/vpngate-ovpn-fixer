747f97ded6de2dbb5946c20ef2da15803dc04920
Park Jaehyung <explainpark101@gmail.com>
2023-07-27 03:13:24 +0900

    added comments to the js file

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-document.querySelectorAll('a[href$=".ovpn"]').forEach(async (el)=>{
-	let data = await fetch(el.href).then(resp=>resp.text());
-	data = data.replaceAll("cipher", "data-ciphers");
-	const href = el.href.split("/").slice(-1)[0];
-	el.download = href;
-	const blob = new Blob([data], { type: 'text/plain' });
-	el.href = window.URL.createObjectURL(blob);
-})
+document.querySelectorAll('a[href$=".ovpn"]') // select all Anchor Element with href ending with ".ovpn"
+.forEach(async (el)=>{ // this function is for all elements. Since fetch api and await keyword needed, used the function as asynchronous one.
+	let data = await fetch(el.href).then(resp=>resp.text()); // fetching the data from the url. This will get the text from the file.
+	data = data.replaceAll("cipher", "data-ciphers"); // replacing "cipher" into "data-ciphers"
+	const href = el.href.split("/").slice(-1)[0]; // getting the name of the file.
+	el.download = href; // setting the name of the downloading file.
+	const blob = new Blob([data], { type: 'text/plain' }); // generates Blob for the file data.
+	el.href = window.URL.createObjectURL(blob); // setting the url for the anchor tag to download the modified data.
+}) // ending function.
